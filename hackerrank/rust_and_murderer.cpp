977b313806ad4cf439810fccb18af0576673578b
Jake Hansen <jhansen44@mail.csuchico.edu>
2018-03-15 15:42:01 -0700

    idk

--- hackerrank/rust_and_murderer.cpp
+++ hackerrank/rust_and_murderer.cpp
@@ -6,9 +6,10 @@
 using namespace std;
 
 typedef vector<long> vl;
-typedef vector< unordered_map<long, bool> > Graph;
+typedef vector<vl> vvl;
+typedef vector< unordered_map<long, bool> > inverse;
 
-void bfs(Graph &G, const long &S, vl &dist) {
+void bfs(vvl &G, const long &S, vl &dist) {
     vector<short> color( G.size() );
     queue<long> Q;
 
@@ -20,9 +21,9 @@ void bfs(Graph &G, const long &S, vl &dist) {
     while (!Q.empty()) {
         u = Q.front();
         Q.pop();
-        for (long v=0; v<(long)G.size(); v++) {
-            // This is a village road to an unvisited node
-            if (v != u && G[u].find(v) == G[u].end() && color[v] == 0) {
+        for (long i=0; i<(long)G[u].size(); i++) {
+            long v = G[u][i];
+            if (color[v] == 0) {
                 color[v] = 1;
                 dist[v] = dist[u] + 1;
                 Q.push(v);
@@ -39,14 +40,20 @@ int main() {
     for (int t=0; t<T; t++) {
         cin >> N >> M;
 
-        Graph G(N);
+        inverse G_inv(N);
         for (int m=0; m<M; m++) {
             cin >> x >> y;
             x--; y--;
 
-            G[x][y] = true;
-            G[y][x] = true;
+            G_inv[x][y] = true;
+            G_inv[y][x] = true;
         }
+
+        vvl G(N);
+        for (long n=0; n<N; n++)
+            for (long q=0; q<N; q++)
+                if (q != n && G_inv[n].find(q) == G_inv[n].end())
+                    G[n].push_back(q);
 
         cin >> S;
         S--;
