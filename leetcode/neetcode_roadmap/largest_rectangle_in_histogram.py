3227b8752a3a75966508c6b075e9377df2ac732c
Jake Hansen <jake@Jakes-MacBook-Pro.local>
2025-04-19 11:30:45 -0700

    FINALLY

--- leetcode/neetcode_roadmap/largest_rectangle_in_histogram.py
+++ leetcode/neetcode_roadmap/largest_rectangle_in_histogram.py
@@ -1,33 +1,32 @@
-from collections import deque
-
-
 class Solution:
     def largestRectangleArea(self, heights: list[int]) -> int:
-        max_rect = 0
+        largest = 0
+        stack = []
+        for cur_pos, cur_height in enumerate(heights):
+            running_height_start = cur_pos
+            running_height = cur_height
+            while len(stack) and stack[-1][1] >= cur_height:
+                # If we found something smaller than the largest thing on the
+                # stack, that means those larger things on the stack have no
+                # hope of forming a rectangle with a future bar of greater or
+                # equal height, so we can just count their max rectangles now
+                running_height_start, running_height = stack.pop(-1)
+                largest = max(largest, (cur_pos - running_height_start) * running_height)
 
-        # Stack holds (position, height)
-        stack = deque([(-1, 0)])
-        for i in range(len(heights)):
-            cur = heights[i]
+            if cur_height != 0:
+                # Since we've popped everything BIGGER than cur_height, that means
+                # the bar of cur_height's rectangle can extend back to the position
+                # of the last bar that was greater than or equal to it
+                stack.append((running_height_start, cur_height))
 
-            while cur < stack[-1][1]:
-                tallest_pos, tallest = stack[-1]
-                stack.pop()
+        # Empty out the rest of the stack
+        for cur_pos, cur_height in stack:
+            rect_size = (len(heights) - cur_pos) * cur_height
+            largest = max(largest, rect_size)
 
-                max_rect = max(max_rect, tallest * (i - stack[-1][0] - 1))
+        return largest
 
-            stack.append((i, cur))
-
-        # Clear-out remaining items
-        while len(stack) > 1:
-            tallest_pos, tallest = stack[-1]
-            stack.pop()
-
-            max_rect = max(max_rect, tallest * (i - stack[-1][0]))
-
-        return max_rect
-
-heights = [2,1,5,6,2,3]
+heights = [1,1,1,1,3]
 
 S = Solution()
 print(S.largestRectangleArea(heights))
